ecb5641ec48d4793db8da3b2fd242aedc85890f9
Expanseman <ethan.blackmail@gmail.com>
2020-07-02 18:53:28 -0700

    adding final for 460

---
+++ CS460 Software Engineering I/FinalExam/Controllers/RefController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalExam.Controllers
+{
+    public class RefController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpGet] use this if updating page form html variables
+        public ActionResult QueryString(int? number)
+        {
+            if (number == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            ViewBag.QS = number;
+
+            return View();
+        }
+    }
+}
